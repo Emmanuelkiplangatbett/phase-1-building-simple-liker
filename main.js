132656536c28ee8650df1fcc0889f9125c805448
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2022-12-08 00:26:08 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModel = document.querySelector("#modal")
+errorModel.classList.add("hidden")
+
+
+const heartClicked = (event) => { 
+
+  mimicServerCall()  
+  .then(()=>{  
+    if(event.target.textContent == EMPTY_HEART){  
+      event.target.textContent = FULL_HEART       
+      event.target.classList.add('activated-heart') 
+    }else{
+      event.target.textContent = EMPTY_HEART     
+      event.target.classList.remove('activated-heart')   
+    }
+
+  })
+  .catch(()=>{  
+    errorModel.classList.remove('hidden')
+    setTimeout(function(){  
+      errorModel.classList.add('hidden') 
+    }, 3000)
+  })
+
+}
+
+
 
 
 
@@ -10,6 +37,12 @@ const FULL_HEART = '♥'
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
+
+let hearts = document.getElementsByClassName('like-glyph') 
+for (let heart of hearts) { 
+  heart.addEventListener('click', heartClicked) 
+
+
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
@@ -22,4 +55,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       }
     }, 300);
   });
-}
+}}
